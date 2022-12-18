f596f7e6e39ddd1046b817bc93036dfceb61e18b
SychevaOlga <113353572+SychevaOlga@users.noreply.github.com>
2022-12-18 22:18:43 +0500

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,9 @@
+# Дан список, вывести отдельно буквы и цифры. 
+
+line = ("a", 'b', '2', '3', 'c')
+
+digits = ''.join([i for i in line if i in '0123456789'])
+letters = ''.join([j for j in line if j not in digits])
+
+print(list(digits))
+print(list(letters))
